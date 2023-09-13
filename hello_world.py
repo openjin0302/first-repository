efda4a0f5b8a4d1543ccfc5a577e3cd90decbe1e
openjin0302 <144299185+openjin0302@users.noreply.github.com>
2023-09-13 15:38:24 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,12 @@
-def print_hello():
-    print("Hello World!!")
+def add(x,y):
+    a = x+y
+    return a
 
 def print_kit():
-    print("kumoh national institute of technology.")
+    print("KIT")
 
 
 if __name__ == '__main__':
-    print_hello()
     print_kit()
+    print(print_hello(1,2))
     
