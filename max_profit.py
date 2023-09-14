42af84c25c8ecc703e17a7d0f98cca3c4395739b
openjin0302 <144299185+openjin0302@users.noreply.github.com>
2023-09-14 14:16:04 +0900

    repair four

--- max_profit.py
+++ max_profit.py
@@ -2,7 +2,7 @@ def maxProfit_bruteforce (prices):
    max_price = 0
 
    for i, price in enumerate(prices):
-       for j in range(i, len(prices)):
+       for jin in range(i, len(prices)):
            max_price = max(prices[j] - price, max_price)
 
    return
